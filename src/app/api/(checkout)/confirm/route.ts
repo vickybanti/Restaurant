7e33469fa916635f4855253c3024667986532102
VICTOR <olamuyiwavictor55@gmail.com>
2024-10-28 15:52:39 +0000

    updated the confirm route

--- src/app/api/(checkout)/confirm/route.ts
+++ src/app/api/(checkout)/confirm/route.ts
@@ -1,14 +1,14 @@
 import { prisma } from "@/lib/utils/connect";
 import { NextResponse } from "next/server";
 
-export const PUT = async (req: Request) => {
+export async function PUT(req: Request) {
   try {
     const { intentId } = await req.json();  // Parsing JSON from request body
     console.log("Intent ID received:", intentId);
 
     if (!intentId) {
-      return new NextResponse(
-        JSON.stringify({ message: "Intent ID is missing" }),
+      return NextResponse.json(
+        { message: "Intent ID is missing" },
         { status: 400 }
       );
     }
@@ -21,24 +21,23 @@ export const PUT = async (req: Request) => {
     });
 
     if (!order) {
-      return new NextResponse(
-        JSON.stringify({ message: "Order not found" }),
+      return NextResponse.json(
+        { message: "Order not found" },
         { status: 404 }
       );
     }
 
     console.log("Order updated:", order);
 
-    return new NextResponse(
-      JSON.stringify({ message: "Order has been updated" }),
+    return NextResponse.json(
+      { message: "Order has been updated" },
       { status: 200 }
     );
   } catch (error) {
     console.error("Error in updating order:", error);
-
-    return new NextResponse(
-      JSON.stringify({ message: "Internal Server Error", error }),
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
       { status: 500 }
     );
   }
-};
+}
